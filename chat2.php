24b752a59a9570f76af12195382f719826e8474e
DjSxX <46792280+DjSxX@users.noreply.github.com>
2020-06-04 09:31:26 +0200

    Update chat2.php
    Add KiwiIRC support to mobile devices

--- chat2.php
+++ chat2.php
@@ -1,29 +1,73 @@
 <?php
-/*
-  Copyright (C) 2015  Kaj Van der Hallen
-
-  This program is free software; you can redistribute it and/or modify
-  it under the terms of the GNU General Public License as published by
-  the Free Software Foundation; either version 2 of the License, or
-  (at your option) any later version.
-
-  This program is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details.
-
-  You should have received a copy of the GNU General Public License along
-  with this program; if not, write to the Free Software Foundation, Inc.,
-  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
-*/
-
 require("includes.php");
 $database = new Database($config);
 $db = $database->getConnection();
 $smarty->assign('fullurl', $config->getFullUrl());
 $smarty->assign('logo', $config->getLogo());
+ 
+$mobile_browser = '0';
+ 
+if (preg_match('/(up.browser|up.link|mmp|symbian|smartphone|midp|wap|phone|android)/i', strtolower($_SERVER['HTTP_USER_AGENT']))) {
+    $mobile_browser++;
+}
+ 
+if ((strpos(strtolower($_SERVER['HTTP_ACCEPT']),'application/vnd.wap.xhtml+xml') > 0) or ((isset($_SERVER['HTTP_X_WAP_PROFILE']) or isset($_SERVER['HTTP_PROFILE'])))) {
+    $mobile_browser++;
+}    
+ 
+$mobile_ua = strtolower(substr($_SERVER['HTTP_USER_AGENT'], 0, 4));
+$mobile_agents = array(
+    'w3c ','acs-','alav','alca','amoi','audi','avan','benq','bird','blac',
+    'blaz','brew','cell','cldc','cmd-','dang','doco','eric','hipt','inno',
+    'ipaq','java','jigs','kddi','keji','leno','lg-c','lg-d','lg-g','lge-',
+    'maui','maxo','midp','mits','mmef','mobi','mot-','moto','mwbp','nec-',
+    'newt','noki','oper','palm','pana','pant','phil','play','port','prox',
+    'qwap','sage','sams','sany','sch-','sec-','send','seri','sgh-','shar',
+    'sie-','siem','smal','smar','sony','sph-','symb','t-mo','teli','tim-',
+    'tosh','tsm-','upg1','upsi','vk-v','voda','wap-','wapa','wapi','wapp',
+    'wapr','webc','winw','winw','xda ','xda-');
+ 
+if (in_array($mobile_ua,$mobile_agents)) {
+    $mobile_browser++;
+}
+    
+if (strpos(strtolower($_SERVER['ALL_HTTP']),'OperaMini') > 0) {
+    $mobile_browser++;
+}
+    
+if (strpos(strtolower($_SERVER['HTTP_USER_AGENT']),'windows') > 0) {
+    $mobile_browser = 0;
+}
+ 
+if ($mobile_browser > 0) {
+	if (isset($_GET['id'])) {
+    $id = $_GET['id'];
+    $chat = new Chatbox($db);
+    if ($chat->getById($id)) {
+        $chat->setCall();
+        // $chat->printConfig();
 
-if (isset($_GET['id'])) {
+        $metadata = $chat->getMetaData();
+
+        $smarty->assign('metadata',$metadata);
+        $smarty->display('kiwi.tpl');
+
+        //if ($metadata['radio'] == "true") {
+        //    $smarty->display('kiwi_radio.tpl');
+        //}
+
+        //$smarty->display('kiwi_end.tpl');
+    }
+    else {
+        $smarty->display('chat_does_not_exist.tpl');
+    }
+}
+else {
+    $smarty->display('chat_does_not_exist.tpl');
+}
+}
+else {
+   if (isset($_GET['id'])) {
     $id = $_GET['id'];
     $chat = new Chatbox($db);
     if ($chat->getById($id)) {
@@ -56,4 +100,6 @@ if (isset($_GET['id'])) {
 else {
     $smarty->display('chat_does_not_exist.tpl');
 }
+}   
+   
 ?>
